4a1a0548f7067ff089cd4bde8d3e687159a23bd4
Akshay Kumar <mail.akshay04@gmail.com>
2021-07-03 17:01:16 +0530

    added review dialog, progressBar fixed theme color issue
    Signed-off-by: Akshay Kumar <mail.akshay04@gmail.com>

--- app/src/main/java/com/akshay/voltticappui/MainActivity.java
+++ app/src/main/java/com/akshay/voltticappui/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact);
+        setContentView(R.layout.charging_review_dialog);
     }
 }
